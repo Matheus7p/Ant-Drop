51620327a2c247a7a5e42e06fe73159fb060ac5e
Matheus7p <matheus.costa.7p@gmail.com>
2025-04-02 17:20:31 -0300

    intial project setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "./ModeToggle";
+
+export default function Header() {
+  return(
+    <header className="flex justify-end p-4">
+      <ModeToggle />
+    </header>
+  )
+}
